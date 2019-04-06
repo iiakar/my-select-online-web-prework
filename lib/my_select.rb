592bfa65527a93a00a5d56dfe432c590f1afe2bc
Ibrahim  Akar <ibsakar@gmail.com>
2019-04-06 23:16:13 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,8 +1,9 @@
-def my_select(collection)
-  
-  my_select(nums) do |number|
- 
- return Proc.new {|n| n*factor }
-
-
-end 
+ def my_collect(collection)
+  i = 0
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i += 1
+  end
+  new_collection
+end
