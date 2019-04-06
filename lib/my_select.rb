773829ffbf28466e4860b1c109a89c3d14047db4
Ibrahim  Akar <ibsakar@gmail.com>
2019-04-06 23:26:59 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,6 @@
  def my_collect(collection)
-  i = 0
+  
   new_collection = []
-  while i < collection.length
-    new_collection << yield(collection[i])
-    i += 1
-  end
-  new_collection
-end
+  my_select(empty_array) do |x|
 
-yield_arg_to("Hello") {|s| s + " World" }
+end 
